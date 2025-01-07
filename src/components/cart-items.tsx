34d5123a8d3ebe708b2193f9eda1eff530b2b43f
ygfest <stefanosanesteban1018@gmail.com>
2025-01-07 22:51:23 +0800

    Added promotional discounts on total products in the cart

--- src/components/cart-items.tsx
+++ src/components/cart-items.tsx
@@ -8,13 +8,34 @@ const CartItems = () => {
   const { removeFromCart, cartItems, handleCloseCart, isOpen } =
     useShoppingCart();
 
+  const totalPrice = cartItems.reduce((total, cartItem) => {
+    const item = productLists.find((product) => product.uuid === cartItem.uuid);
+    const productPrice = item ? parseFloat(item.price) : 0;
+    return total + productPrice * cartItem.quantity;
+  }, 0);
+
+  let discountedPrice = totalPrice;
+  let discountApplied = "None";
+  if (totalPrice > 100) {
+    discountedPrice = totalPrice - totalPrice * 0.2;
+    discountApplied = "20% off on total greater than $100";
+  } else if (totalPrice > 50) {
+    discountedPrice = totalPrice - totalPrice * 0.15;
+    discountApplied = "15% off on total greater than $50";
+  } else if (totalPrice > 20) {
+    discountedPrice = totalPrice - totalPrice * 0.1;
+    discountApplied = "10% off on total greater than $20";
+  }
+
   return (
     <>
       {isOpen && cartItems.length > 0 && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-end z-50">
           <div className="bg-white w-full md:max-w-lg right-0 top-0 bottom-0 shadow-lg overflow-hidden">
             <div className="flex justify-between items-center p-4 border-b border-gray-300">
-              <h2 className="text-xl font-bold text-gray-800">Shopping Cart</h2>
+              <h2 className="text-xl font-bold text-gray-800">
+                Products in Shopping Cart
+              </h2>
               <button
                 className="text-gray-500 hover:text-gray-800"
                 onClick={handleCloseCart}
@@ -68,21 +89,33 @@ const CartItems = () => {
 
             {cartItems.length > 0 && (
               <div className="p-4 border-t border-gray-300">
-                <div className="flex justify-between items-center">
-                  <h3 className="text-lg font-semibold text-gray-800">Total</h3>
-                  <p className="text-xl font-bold text-gray-900">
-                    {formatCurrency(
-                      cartItems.reduce((total, cartItem) => {
-                        const product = productLists.find(
-                          (product) => product.uuid === cartItem.uuid
-                        );
-                        const productPrice = product
-                          ? parseFloat(product.price)
-                          : 0;
-                        return total + productPrice * cartItem.quantity;
-                      }, 0)
-                    )}
-                  </p>
+                <div className="flex flex-col gap-2 justify-between items-center">
+                  <div className="flex w-full justify-between">
+                    <h3 className="text-lg font-semibold text-gray-800">
+                      Total:
+                    </h3>
+                    <p className="text-xl font-bold text-gray-900">
+                      {formatCurrency(totalPrice)}
+                    </p>
+                  </div>
+                  <div className="flex w-full justify-between">
+                    <h3 className="text-lg font-semibold text-gray-800">
+                      Discount Applied:
+                    </h3>
+                    <p className="text-md font-normal text-gray-500">
+                      {discountApplied}
+                    </p>
+                  </div>
+
+                  <div className="flex w-full justify-between">
+                    {" "}
+                    <h3 className="text-lg font-semibold text-gray-800">
+                      Discounted Price:
+                    </h3>
+                    <p className="text-xl font-bold text-gray-900">
+                      {formatCurrency(discountedPrice)}
+                    </p>
+                  </div>
                 </div>
               </div>
             )}
